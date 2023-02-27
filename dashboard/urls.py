c9234e15c9d9ed7943d4d6c41ab4dd9af280c496
xolani863 <mhlangaxlolani21@gmail.com>
2023-02-27 17:56:05 +0200

    Models for the Homework Section

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home)
+    path('',views.home,name="home"),
+    # path('notes',views.notes, name="notes"),
+    # path('delete_note/<int:pk>', views.delete_note,name="delete_note"),
+    path('homework',views.homework, name="homework")
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
+from . forms  import *
+from django.contrib import messages
+from django.views import generic
 
 # Create your views here.
 def home(request):
     return render(request, 'dashboard/home.html')
+
+# def notes(request):
+#     if request.method == "POST":
+#         form = NotesForm(request.POST)
+#         if form.is_valid():
+#             notes = Notes(user=request.user,title=request.POST['title'],description=request.POST['description'])
+#             notes.save()
+#             messages.success(request,f"Notes Added from{request.user.username} Successfully")
+
+#     else:       
+#          form = NotesForm()
+#          notes = Notes.objects.filter(user=request.user)
+#          context = {'notes':notes, 'form':form}
+#          return render(request, 'dashboard/notes.html',context)
+    
+
+def homework(request):
+    return render(request, 'dashboard/homework.html')
+
+
 
